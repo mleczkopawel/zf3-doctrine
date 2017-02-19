aeb50ecb87061bafd9d361212f4087ac16111a9b
mleczkopawel <mleczko.pawel1@gmail.com>
2017-02-19 22:49:35 +0100

    Update providers.php

--- config/providers.php
+++ config/providers.php
@@ -8,14 +8,14 @@
 
 return [
     'fb' => [
-        'clientId' => '1143277982455317',
-        'clientSecret' => '50e5a58909bba2d062525649954aa37a',
+        'clientId' => 'id',
+        'clientSecret' => 'secret',
         'redirectUri' => 'https://' . $_SERVER['HTTP_HOST'] . '/' . LOCALE . '/auth/callback/fb',
         'graphApiVersion' => 'v2.8'
     ],
     'google' => [
-        'clientId' => '171739267348-a9iq068th25opq6itdgea63qj8c6326k.apps.googleusercontent.com',
-        'clientSecret' => 'cCeql-sXixl__DL03YWTuU7x',
+        'clientId' => 'id',
+        'clientSecret' => 'secret',
         'redirectUri' => 'https://' . $_SERVER['HTTP_HOST'] . '/' . LOCALE . '/auth/callback/google',
         'hostedDomain' => 'https://zf3d.pl'
     ]
